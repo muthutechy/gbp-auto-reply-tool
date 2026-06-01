bcab033b75a9a6987ffeca518970f56676daed03
Muthukrishnan2002 <muthuclashofclans001@gmail.com>
2026-06-01 11:04:24 +0530

    temporary cors debug

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,22 +8,13 @@ const { notFound, errorHandler } = require("./middleware/error.middleware");
 
 const app = express();
 
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:3000",
-].filter(Boolean);
-
 app.use(
   cors({
-    origin(origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error(`Origin ${origin} not allowed by CORS`));
-    },
+    origin: true,
     credentials: true,
   })
 );
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
